9d1054b5aa95963402c14fdf1bf923f2780dfd73
Will Strinz <wstrinz@gmail.com>
2013-11-30 12:48:20 -0600

    basic primecoin setup/dependencies

--- cookbooks/primecoin/recipes/default.rb
+++ cookbooks/primecoin/recipes/default.rb
@@ -1,15 +1,39 @@
-%w{git make g++ build-essential libminiupnpc-dev}.each do |pkg|
+vagr = '/home/vagrant'
+
+execute "update source" do
+  command "sudo apt-get update"
+end
+
+%w{screen curl yasm git make g++ build-essential libminiupnpc-dev  libdb++-dev libgmp-dev libssl-dev dos2unix}.each do |pkg|
   package pkg do
     action :install
   end
 end
 
-package "curl" do
-  action :install
+
+execute "dependancy checkpoint 1" do
+  command "echo hello"
 end
 
-directory "miners" do
+%w{libboost-all-dev libboost-chrono1.48-dev libboost-filesystem1.48-dev libboost-program-options1.48-dev libboost-thread1.48-dev}.each do |p|
+  package p do
+    action:install
+  end
+end
+
+directory "#{vagr}/miners" do
   owner "vagrant"
   mode "0755"
   action :create
+end
+
+execute "clone primecoin" do
+  cwd "#{vagr}/miners"
+  command "git clone https://github.com/thbaumbach/primecoin"
+  not_if { ::File.exists?("#{vagr}/miners/primecoin")}
+end
+
+execute "build from source" do
+  cwd "#{vagr}/miners/primecoin/src"
+  command "make -f makefile.unix"
 end
